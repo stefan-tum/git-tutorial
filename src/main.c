0fcebfd4a544f5fd5bcd08ca39be691c8ca87eb8
stefans2 <stefans2@illinois.edu>
2023-04-25 11:40:15 +0200

    Completed Coding Challenge 2 on merging branch, merging to master

--- src/main.c
+++ src/main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include "espl_lib.h"
 
 int main(){
 
-	printf("Hello ESPL\n");
-	printf("This is a new message");
+	int input1, input2;
+	char *output;
+	do{
+	printf("\nPlease input an integer:\n");
+	scanf("%d", &input1);
+
+	printf("\nYour number is: ");
+	output = num_to_words(input1);
+	printf(output);
+
+	printf("\nIf you wish to exit the program, please enter 0, to run again, enter an integer other than 0:\n");
+	scanf("%d", &input2);
+
+	//printf("Hello ESPL\n");
+	//printf("This is a new message");
+	}while(input2);
 	return 0;
 
 }
